67c3405b854f10eb2589f1f012939979c41c6125
Logan Alvarez <logan.alvarez616@gmail.com>
2022-01-13 15:55:37 -0500

    Added files and interface

---
+++ src/components/PostInList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Post from "../models/postInterface";
+
+interface postProps {
+  post: Post;
+}
+
+function PostInList({ post }: postProps) {
+  return <div></div>;
+}
---
+++ src/components/SocialPosts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "../models/postInterface";
+
+function SocialPosts() {
+  return (
+    <div className="spcontainer">
+      <h1>My Thoughts</h1>
+
+      <button className="newThought">New Thought</button>
+    </div>
+  );
+}
+
+export default SocialPosts;
